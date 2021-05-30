8f53ec11f8fd3535a0d9a88812f74d29d08efad5
Adrian Flutur <ady_flutur@yahoo.com>
2021-05-31 01:22:52 +0300

    Refactored tests. Added a password strength checker

---
+++ lib/src/password_strength_checker.dart
@@ -0,0 +1,62 @@
+import 'package:random_string_generator/random_string_generator.dart';
+
+/// The strength of a password.
+///
+/// You will receive a value from this enum after calling
+/// [PasswordStrengthChecker.checkStrength(password)].
+enum PasswordStrength {
+  VERY_WEAK,
+  WEAK,
+  GOOD,
+  STRONG,
+}
+
+/// The RegEx pattern to enforce for loose, medium and tight
+/// passwords.
+///
+/// The password will be matched against all three patterns
+/// in order to figure out how strong is the password.
+class EnforcingPattern {
+  /// Pattern for a weak password.
+  final String loosePattern;
+
+  /// Pattern for a medium password.
+  final String mediumPattern;
+
+  /// Pattern for a strong password.
+  final String tightPattern;
+
+  /// Constructor
+  const EnforcingPattern({
+    this.loosePattern = r'(?=.*[a-z])(?=.*[A-Z])(?=.{6,})',
+    this.mediumPattern = r'((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{6,}))'
+        '|'
+        '((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))',
+    this.tightPattern = r'(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})',
+  });
+}
+
+/// This class is used to check how secure is a password generated
+/// with [RandomStringGenerator].
+class PasswordStrengthChecker {
+  /// Returns a value of type [PasswordStrength], which is how
+  /// secure is the given password, according to the provided [EnforcingPattern].
+  static PasswordStrength checkStrength(
+    String password, {
+    EnforcingPattern enforcingPattern = const EnforcingPattern(),
+  }) {
+    PasswordStrength strength;
+
+    if (RegExp(enforcingPattern.tightPattern).hasMatch(password)) {
+      strength = PasswordStrength.STRONG;
+    } else if (RegExp(enforcingPattern.mediumPattern).hasMatch(password)) {
+      strength = PasswordStrength.GOOD;
+    } else if (RegExp(enforcingPattern.loosePattern).hasMatch(password)) {
+      strength = PasswordStrength.WEAK;
+    } else {
+      strength = PasswordStrength.VERY_WEAK;
+    }
+
+    return strength;
+  }
+}
--- test/random_string_generator_test.dart
+++ test/random_string_generator_test.dart
@@ -2,18 +2,11 @@ import 'package:random_string_generator/random_string_generator.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    late RandomStringGenerator generator;
+  group('Password generator tests', () {
+    test('Test that password length are correct', () {
+      var generator = RandomStringGenerator(fixedLength: 10, hasDigits: false);
 
-    setUp(() {
-      generator = RandomStringGenerator(
-        minLength: 5,
-        maxLength: 25,
-      );
-    });
-
-    test('First Test', () {
-      bool checkStringLength(String str, int min, int max) {
+      bool hasLengthBetween(String str, int min, int max) {
         return str.length <= max && str.length >= min;
       }
 
@@ -22,7 +15,6 @@ void main() {
       Iterable.generate(10).forEach((_) {
         try {
           var string = generator.generate();
-          print(string);
           generatedList.add(string);
         } on RandomStringGeneratorException catch (e) {
           print(e.message);
@@ -31,14 +23,12 @@ void main() {
 
       var ok = true;
       for (var string in generatedList) {
-        ok = checkStringLength(string, 5, 25);
+        ok = hasLengthBetween(string, 5, 25);
       }
 
       expect(ok, isTrue);
     });
-  });
 
-  group('Specific tests', () {
     test('Test hasDigits parameter set to false', () {
       var generator = RandomStringGenerator(fixedLength: 10, hasDigits: false);
       final newValue = generator.generate();
@@ -51,6 +41,46 @@ void main() {
       final newValue = generator.generate();
       final hasDigit = newValue.contains(RegExp(r'[0-9]'));
       expect(hasDigit, true);
+    });
+  });
+
+  group('Password checker tests', () {
+    test('Test weak password', () {
+      final generator = RandomStringGenerator(
+        fixedLength: 6,
+        hasAlpha: true,
+        hasDigits: false,
+        hasSymbols: false,
+      );
+      final password = generator.generate();
+      final strength = PasswordStrengthChecker.checkStrength(password);
+      expect(strength, PasswordStrength.WEAK);
+    });
+
+    test('Test good password', () {
+      final generator = RandomStringGenerator(
+        minLength: 6,
+        maxLength: 10,
+        hasAlpha: true,
+        hasDigits: true,
+        hasSymbols: false,
+      );
+      final password = generator.generate();
+      final strength = PasswordStrengthChecker.checkStrength(password);
+      expect(strength, PasswordStrength.GOOD);
+    });
+
+    test('Test strong password', () {
+      final generator = RandomStringGenerator(
+        minLength: 8,
+        maxLength: 12,
+        hasAlpha: true,
+        hasDigits: true,
+        hasSymbols: true,
+      );
+      final password = generator.generate();
+      final strength = PasswordStrengthChecker.checkStrength(password);
+      expect(strength, PasswordStrength.STRONG);
     });
   });
 }
